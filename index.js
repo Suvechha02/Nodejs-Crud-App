7393c18cce3fed663d93a0e31708761bc2bb8126
Suvechha <151946319+Suvechha02@users.noreply.github.com>
2024-06-06 11:38:48 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require ("express");
-
-const app = express();
-
-app.get("/test",(req,res)=>{
-    res.send("<h2>Welcome to App>/h2>");
-})
-const PORT = 8080;
-app.listen("server listening at port no :",PORT);
